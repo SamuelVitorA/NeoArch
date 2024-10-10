a57e27e2ddc22d264b1062f0341ab15f6a6922b8
SamuelV <samuelvitoramaralsantossantos@gmail.com>
2024-10-10 17:40:27 -0300

    a

--- src/pages/ADM/configuracoes/index.jsx
+++ src/pages/ADM/configuracoes/index.jsx
@@ -1,12 +1,84 @@
 import './index.scss';
 
+
 import { useState } from 'react';
 
 
 export default function Config() {
+
+    const [ nome, setNome ] = useState('');
+    const [ cpf, setCpf ] = useState('');
+    const [ senha, setSenha ] = useState('');
+    const [ enderesso, setEnderesso ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ telefone, setTelefone ] = useState('');
+    const [ imagem, setImagem ] = useState('');
+
     return(
         <div className='config'>
+            <h1>Personal information</h1>
 
+            <hr/>
+
+            <div className='foto'>
+                <img src="./assets/images/do-utilizador.png" alt="" />
+
+                <h6>Mudar foto de perfil</h6>
+            </div>
+            
+            <div className='quadrado'>
+                <coluna_esq>
+                    <div>
+                        <h1>Name</h1>
+
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <h1>Password</h1>
+
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <h1>E-mail</h1>
+
+                        <input type="text" />
+                    </div>
+                </coluna_esq>
+        
+                <coluna_dir>
+
+
+                    <div>
+                        <h1>CPF</h1>
+
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <h1>Address</h1>
+
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <h1>telephone</h1>
+
+                        <input type="text" />
+                    </div>
+                </coluna_dir>
+
+            </div>
+
+
+
+            <div className="acessibilidade">
+            <h1>Personal information</h1>
+
+            <hr/>
+
+            </div>
         </div>
     )
-}
+};
--- src/pages/ADM/home/index.jsx
+++ src/pages/ADM/home/index.jsx
@@ -7,15 +7,23 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return(
         <div className='home'>
-              <Link to={"/analitic"}>analitic</Link>  
-              <Link to={"/pedidos"}>pedidos</Link> 
-              <Link to={"/config"}>config</Link> 
+              <Link to={"/analitic"}>Analitic</Link>  
+
+              <Link to={"/pedidos"}>Pedidos</Link> 
+
+              <Link to={"/config"}>Config</Link> 
+
               <Link to={"/cadastro"}>Cadastro</Link> 
+
               <Link to={"/login"}>Login</Link> 
-              <Link to={"/Esqueceu_senha"}>Esqueceu_senha</Link> 
+
               <Link to={"/Mudar_senha"}>Mudar_senha</Link> 
+
               <Link to={"/Main_automo"}>Main_automo</Link> 
+
               <Link to={"/Main_empresa"}>Main_empresa</Link> 
+
+              <Link to={"/Esqueceu_senha"}>Esqueceu_senha</Link> 
         </div>        
     )
 }
