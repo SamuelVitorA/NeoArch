1cccd76d96999c1d3833bf26093c8ac128c63e5e
Samuel <samuelvitoramaralsantossantos@gmail.com>
2024-10-15 16:35:42 -0300

    config

--- src/pages/ADM/configuracoes/index.jsx
+++ src/pages/ADM/configuracoes/index.jsx
@@ -28,44 +28,43 @@ export default function Config() {
             
             <div className='quadrado'>
                 <coluna_esq>
-                    <div>
+                    <div className='ab'>
                         <h1>Name</h1>
-
-                        <input type="text" />
+                        <input type="text" placeholder='Samuel Vitor' />
                     </div>
 
-                    <div>
+                    <div className='ab'>
                         <h1>Password</h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder='1234...'/>
                     </div>
 
-                    <div>
+                    <div className='ab'>
                         <h1>E-mail</h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder='samuelvitor@gmail.com' />
                     </div>
                 </coluna_esq>
         
                 <coluna_dir>
 
 
-                    <div>
+                    <div className='ab'>
                         <h1>CPF</h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder='000.000.000-00'/>
                     </div>
 
-                    <div>
+                    <div className='ab'>
                         <h1>Address</h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder='Rua café...' />
                     </div>
 
-                    <div>
+                    <div className='ab'>
                         <h1>telephone</h1>
 
-                        <input type="text" />
+                        <input type="text" placeholder='11 00000-0000'/>
                     </div>
                 </coluna_dir>
 
@@ -74,10 +73,40 @@ export default function Config() {
 
 
             <div className="acessibilidade">
-            <h1>Personal information</h1>
+                <h1 className='a'>Accessibility</h1>
 
-            <hr/>
+                <hr/>
 
+                <div className='ativar'>
+                    <h3>Activate Notification</h3>
+                    <div className="toggle">
+                            <input type="checkbox"/>
+                    </div>
+                </div>
+
+                <hr />
+                
+                <div className='ativar'>
+                    <h3>Night mode</h3>
+                    <div className="toggle">
+                            <input type="checkbox"/>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className='ativar'>
+                    <h3>Change language</h3>
+                    <h3>Change</h3>
+                </div>
+                <hr />
+                
+                <div className='linha'> 
+                    <h2>Register Account</h2>
+                    <h2>Choose Account</h2>
+                    <h2 className='red'>Delete Account</h2>
+                </div>
+                
             </div>
         </div>
     )
