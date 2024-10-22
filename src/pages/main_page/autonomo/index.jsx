5047c5cde66c7dcb407323ce8706b13823939155
pedrolrcm <pedro.leopoldino.rcm@gmail.com>
2024-10-22 00:46:40 -0300

    mudanças na tela de loginn

--- src/pages/main_page/autonomo/index.jsx
+++ src/pages/main_page/autonomo/index.jsx
@@ -1,15 +1,31 @@
 import './index.scss';
+import { register } from 'swiper/element';
+
+
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { useState, useNavigate } from "react";
 import videojs from 'video.js';
 
-export default function Main_automo() {
-    const [video] = useState('./assets/videos/video1carrossel');
-    const [imagem] = useState('./assets/images/logowhite.png')
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+register();
+
+
+
+export default function Main_automo(){
+    const data = [
+        {id:'1', video :'./assets/videos/video1carrossel.mp4'},
+        {},
+        {},
+    ]
        return(
             <div className="aut">
                 <div className='header'>
-                    <img className='logo' src={imagem} alt="" />
+                    <img src="./assets/images/logowhite.png" alt="" className='logo'/>
                     <div className='navigate-control'>
                         <h3>Home</h3>
                         <h3>Projects</h3>
@@ -18,9 +34,24 @@ export default function Main_automo() {
                     </div>
                 </div>
                 <div className='section1'>
+                    <Swiper
+                    slidesPerView={1}
+                    pagination={{clickable:true}}
+                    navigation
+                    >
+                        {data.map( (item)=>(
+                        <SwiperSlide key={item.id}>
+                            <video 
+                            src={item.image}
+                            alt="Slider"
+                            className='slider-item'
+                             />
+                        </SwiperSlide>
+                        )
+                    )}
+                    </Swiper>
                 
 
-                
                 </div>
 
             </div>
