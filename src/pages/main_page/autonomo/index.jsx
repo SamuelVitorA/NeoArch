62f00f95904ff798134b75be03717c20c75707db
pedrolrcm <pedro.leopoldino.rcm@gmail.com>
2024-11-01 01:12:43 -0300

    tenntativa falha

--- src/pages/main_page/autonomo/index.jsx
+++ src/pages/main_page/autonomo/index.jsx
@@ -3,31 +3,39 @@ import { Link } from 'react-router-dom';
 
 
 export default function Main_automo(){
-       return(
-            <div className="aut">
-                <header>
-                    <Link to="/"><img src="./assets/images/logowhite.png" alt="" className='logo'/></Link>
-                    <div className='navigate-control'>
-                        <h3>Our services</h3>
-                        <h3>Contact</h3>
-                        <Link to="/login"><h3>Login</h3></Link>
-                    </div>
-                    <div className="last-navigate" >
-                        <h2>Get a conference</h2>
+    return(
+        <div className="aut">
+            <header>
+                <Link to="/"><img src="./assets/images/logowhite.png" alt="" className='logo'/></Link>
 
-                    </div>
-                </header>
-                <section className="section1">
-                    <div className="video-background">
-                        <video autoPlay loop muted>
-                        <source src="./assets/videos/videoteste.mp4" type="video/mp4" />
-                        </video>
-                    </div>
-                    
+                <div className='navigate-control'>
+                    <h3>Our services</h3>
+                
+                    <h3>Contact</h3>
+                
+                    <Link to="/login"><h3>Login</h3></Link>
+                </div>
+                
+                <div className="last-navigate" >
+                    <h2>Get a conference</h2>
+                </div>
+            </header>
 
-                </section>
-            </div>
-        )
-    
-    }
+            <section className="section1">
+                <div className="video-background">
+                    <video autoPlay loop muted className="video">
+                        <source src="./assets/videos/videoooo.mp4" type="video/mp4" />
+                    </video>
+                </div>
+            </section>            
 
+            <section_2>
+                <h1>Testando meu carogo</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aliquid odit nostrum nemo fugiat maxime, consequuntur aspernatur, illum ea, beatae quod fuga quia molestias est alias laboriosam ipsam doloribus veniam?</p>
+            </section_2>
+        </div>
+
+    )
+
+}
